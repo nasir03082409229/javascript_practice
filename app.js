aedff6cd6401e22f934fc7e51f1a9391c4c80028
Muhammad Nasir <nasir03082409229@gmail.com>
2017-03-07 03:27:07 -0800

    2nd class javaScript

---
+++ app.js
@@ -0,0 +1,19 @@
+var per = +prompt("Enter Your Percentage","Type Here !");
+// var urdu=+prompt("Enter urdu marks","marks");
+
+if(per >=80)
+{
+    alert("your Grade is A+");
+}else if(per >60 && per<=70)
+{
+    alert("your Grade is A");       
+}else if(per >50 && per<=60)
+{
+    alert("your Grade is B");       
+} else if(per >40 && per<=50)
+{
+    alert("your Grade is C");       
+}else if(per<=49)
+{
+    alert("your Grade is F");       
+}   
